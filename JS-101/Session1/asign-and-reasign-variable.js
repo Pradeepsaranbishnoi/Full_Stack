ff42b413636cef8dc242426226a2a7a9f3dcbd2c
Pradeep Saran <pradeepmadasar@gmail.com>
2024-09-30 21:51:59 +0530

    assign varibal enad re assign var

---
+++ JS-101/Session1/asign-and-reasign-variable.js
@@ -0,0 +1,5 @@
+var temp;
+temp = "Pradeep";
+console.log(temp);
+temp = 24;
+console.log(temp);
---
+++ JS-101/Session1/greeter-variable.js
@@ -0,0 +1,4 @@
+var greeter = "hey hi"; // Initialization
+console.log(greeter); // Output: hey hi
+var greeter = "Hey"; // Reassignment
+console.log(greeter);
