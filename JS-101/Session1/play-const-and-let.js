556ed04d89aaaab684433328b40bb17f9593ed8d
Pradeep Saran <pradeepmadasar@gmail.com>
2024-09-30 22:34:35 +0530

    let and var decalaration

---
+++ JS-101/Session1/play-const-and-let.js
@@ -0,0 +1,15 @@
+let greeting = "say Hi";
+console.log(greeting);
+
+// //Redeclare
+// let greeting = "say Hello"; // This will cause an error
+// console.log(greeting);
+
+const greet = "say Hi";
+console.log(greet);
+
+// //Reassign
+// greeting = "say Hello"; // This will cause an error
+
+// //Re-declare
+// const greeting = "say Hey"; // This will cause an error
